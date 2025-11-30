64329068e1caba9cd10f4eb765dfb64edcb92524
Sultan Mia <sultanmia5732@gmail.com>
2025-11-30 16:13:09 +0600

    create and setup mongodb

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = 3000
+
+//MIDLEWARE 
+app.use(cors())
+app.use(express.json())
+
+
+const uri =process.env.DATABASE_URI 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+
+
+async function run() {
+  try {
+   
+    await client.connect();
+   
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
